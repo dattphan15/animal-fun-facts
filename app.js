856bec376f0976914ab99188dcebcce0b32ac4f5
dattphan15 <ken.phan@live.ca>
2021-06-30 15:56:49 -0700

    add a title

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "";
+const animalFacts = (
+  <h1>
+    { title === "" ? 'Click an animal for a fun fact!' : title }
+  </h1>
+);
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
