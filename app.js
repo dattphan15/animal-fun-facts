19cb1c0854644aeda33ea878e47e9a928d7457e2
dattphan15 <ken.phan@live.ca>
2021-06-30 16:23:24 -0700

    add an array of images

--- app.js
+++ app.js
@@ -3,14 +3,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = "";
-const background = <img className="background" alt="ocean" src="/images/ocean.jpg"/>;
+const background = (
+  <img 
+    className="background" 
+    alt="ocean" 
+    src="/images/ocean.jpg"
+  />
+);
+
+let images = [];
+
+for (const animal in animals) {
+  images.push( 
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      ariaLabel={animal}
+      role='button'
+    />
+  )
+}
 
 const animalFacts = (
   <div>
     <h1>
       { title === "" ? 'Click an animal for a fun fact!' : title }
     </h1>
-    {background}
+    { background }
+    <div className="animals">
+      { images }
+    </div>
   </div>
 );
 
